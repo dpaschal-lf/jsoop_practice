da613778f682210bff5dabc62f056ae5725106e9
Tim Horist <TimelikeClosure@users.noreply.github.com>
2019-01-18 13:03:15 -0800

    fs3 updated requirements to verify add and remove receive non-negative numbers

--- exercises/bankaccount.js
+++ exercises/bankaccount.js
@@ -7,11 +7,15 @@ class Account{
 	add(  ){
 		//add money to the amount stored in the account
 		//takes in an amount
+		//checks if it is actually a number greater than 0
+			//if not, return false
 		//adds it to the existing amount
 		//returns the new amount in the account
 	}
 	remove( amount ){
 		//removes funds from an account
+		//checks if amount is actually a number greater than 0
+			//if not, return false
 		//check if the amount to withdraw is more than the account
 		//if more, only withdraw the amount in the account, not more
 		//if less, withdraw the amount specified
